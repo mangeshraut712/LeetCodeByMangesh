7f46e181380d4704cbcbe10a60e08b4ae14c9f6b
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-04-09 21:11:35 -0400

    Time: 59 ms (30.46%) | Memory: 22.2 MB (21.97%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,27 +1,17 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # Base case: if root is None, return None
         if not root:
             return None
         
-        # If root is either p or q, return root (node can be its own descendant)
         if root == p or root == q:
             return root
+            
+        left_lca = self.lowestCommonAncestor(root.left, p, q)
+        right_lca = self.lowestCommonAncestor(root.right, p, q)
         
-        # Recursively search left and right subtrees
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        
-        # If both left and right are non-None, root is the LCA
-        if left and right:
+        if left_lca and right_lca:
             return root
-        
-        # If only one side is non-None, return that side
-        return left if left else right
+        elif left_lca:
+            return left_lca
+        else:
+            return right_lca
