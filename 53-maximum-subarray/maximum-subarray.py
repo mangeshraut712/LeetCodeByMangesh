ce1c6a85ac6f845d66c79249737078bf1294c539
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-04-09 21:29:51 -0400

    Time: 100 ms (10.59%) | Memory: 32.9 MB (6.99%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,17 @@
+from typing import List
+import math
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if not nums:
+            return 0 # Or handle as per constraints/edge case definition
+            
+        current_max_sum = nums[0]
+        global_max_sum = nums[0]
+        
+        for i in range(1, len(nums)):
+            num = nums[i]
+            current_max_sum = max(num, current_max_sum + num)
+            global_max_sum = max(global_max_sum, current_max_sum)
+            
+        return global_max_sum
