f591f8f76b1a4d43963d02cb14e4c712f2d2f44c
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-04-09 21:46:11 -0400

    Time: 3 ms (98.15%) | Memory: 19.2 MB (93.21%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -2,9 +2,6 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-            
         left, right = 0, len(height) - 1
         left_max, right_max = 0, 0
         total_water = 0
