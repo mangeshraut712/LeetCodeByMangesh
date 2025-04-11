2921dbfa0819185daea1bc7ac11f8b566be19d8d
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-04-10 21:04:28 -0400

    Time: 11 ms (38.28%) | Memory: 17.8 MB (84.91%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,29 +1,20 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
-        memo = {}
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
         
-        def dp(i: int, j: int) -> bool:
-            if (i, j) in memo:
-                return memo[(i, j)]
-            
-            # Base cases
-            if j == n:
-                return i == m
-            if i > m:
-                return False
-            
-            # First characters match
-            first_match = i < m and (p[j] == s[i] or p[j] == '.')
-            
-            if j + 1 < n and p[j + 1] == '*':
-                # '*' can match zero or one/more
-                result = (dp(i, j + 2) or  # Zero match
-                         (first_match and dp(i + 1, j)))  # One or more match
-            else:
-                result = first_match and dp(i + 1, j + 1)
-            
-            memo[(i, j)] = result
-            return result
-        
-        return dp(0, 0)
+        dp[0][0] = True
+        for j in range(1, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+                
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '.' or p[j - 1] == s[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    zero_match = dp[i][j - 2]
+                    one_or_more_match = (p[j - 2] == '.' or p[j - 2] == s[i - 1]) and dp[i - 1][j]
+                    dp[i][j] = zero_match or one_or_more_match
+                    
+        return dp[m][n]
