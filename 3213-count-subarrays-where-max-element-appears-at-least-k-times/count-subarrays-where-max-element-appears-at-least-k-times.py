d60a7b06ec01b1bf1dcbe89ebfa8eddbf2152f84
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-04-30 00:39:15 -0400

    Time: 100 ms (31.57%) | Memory: 29 MB (99.53%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,21 +1,18 @@
+from typing import List
 from collections import deque
 
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        max_elem = max(nums)
+        max_val = max(nums)
+
         count = 0
-        left = 0
-        freq = 0
+        last_max_indices = deque()
 
         for right in range(len(nums)):
-            if nums[right] == max_elem:
-                freq += 1
+            if nums[right] == max_val:
+                last_max_indices.append(right)
 
-            while freq >= k:
-                # All subarrays starting at `left` and ending at `right`, `right+1`, ..., `n-1` are valid
-                count += len(nums) - right
-                if nums[left] == max_elem:
-                    freq -= 1
-                left += 1
+            if len(last_max_indices) >= k:
+                count += last_max_indices[len(last_max_indices) - k] + 1
 
         return count
