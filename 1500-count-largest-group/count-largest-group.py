a799dfbb1314e57fb6cd04b1bf86494f9f06a0bc
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-05-02 15:04:38 -0400

    Time: 19 ms (92.95%) | Memory: 17.7 MB (63.85%) - LeetSync

--- 1500-count-largest-group/count-largest-group.py
+++ 1500-count-largest-group/count-largest-group.py
@@ -1,7 +1,18 @@
-from collections import Counter
-
 class Solution:
     def countLargestGroup(self, n: int) -> int:
-        cnt = Counter(sum(map(int, str(i))) for i in range(1, n+1))
-        M = max(cnt.values())
-        return sum(v == M for v in cnt.values())
+        arr = [0] * 37
+        for i in range(1, n + 1):
+            x, s = i, 0
+            while x:
+                s += x % 10
+                x //= 10
+            arr[s] += 1
+        M = 0
+        cnt = 0
+        for v in arr:
+            if v > M:
+                M = v
+                cnt = 1
+            elif v == M:
+                cnt += 1
+        return cnt
