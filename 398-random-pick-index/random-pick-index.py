8a25cb8db67953cdc0051d83e06ece8579aa4af9
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-05-02 03:44:00 -0400

    Time: 43 ms (54.70%) | Memory: 28 MB (46.57%) - LeetSync

--- 398-random-pick-index/random-pick-index.py
+++ 398-random-pick-index/random-pick-index.py
@@ -1,19 +1,11 @@
 import random
-from typing import List
 from collections import defaultdict
 
 class Solution:
-    def __init__(self, nums: List[int]):
-        self.indices = defaultdict(list)
-        for i, num in enumerate(nums):
-            self.indices[num].append(i)
+    def __init__(self, nums):
+        self.d = defaultdict(list)
+        for i, x in enumerate(nums):
+            self.d[x].append(i)
 
-    def pick(self, target: int) -> int:
-        # Retrieve the list of indices for the target number
-        target_indices = self.indices[target]
-        # Randomly select an index from the list
-        return random.choice(target_indices)
-
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(nums)
-# param_1 = obj.pick(target)
+    def pick(self, t):
+        return random.choice(self.d[t])
