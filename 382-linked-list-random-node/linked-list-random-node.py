5eb8f2902ffddef4442037a641c1afbe60d8aefd
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-05-02 03:48:58 -0400

    Time: 13 ms (69.41%) | Memory: 20.4 MB (73.15%) - LeetSync

--- 382-linked-list-random-node/linked-list-random-node.py
+++ 382-linked-list-random-node/linked-list-random-node.py
@@ -1,23 +1,12 @@
 import random
-from typing import Optional
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 
 class Solution:
-    def __init__(self, head: Optional[ListNode]):
-        self.head = head
+    def __init__(self, head):
+        self.a = []
+        while head:
+            self.a.append(head.val)
+            head = head.next
+        self.c = random.choice
 
     def getRandom(self) -> int:
-        node = self.head
-        ans = 0
-        i = 1
-        while node:
-            if random.randrange(i) == 0:
-                ans = node.val
-            node = node.next
-            i += 1
-        return ans
+        return self.c(self.a)
