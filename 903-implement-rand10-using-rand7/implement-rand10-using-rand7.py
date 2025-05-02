153f7da237c5137a233a85998f1c51c51eab1a09
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-05-02 03:29:23 -0400

    Time: 173 ms (62.91%) | Memory: 20.2 MB (42.72%) - LeetSync

--- 903-implement-rand10-using-rand7/implement-rand10-using-rand7.py
+++ 903-implement-rand10-using-rand7/implement-rand10-using-rand7.py
@@ -1,6 +1,7 @@
 class Solution:
     def rand10(self):
         r7 = rand7
-        while (r := (r7() - 1) * 7 + r7()) > 40:
-            pass
-        return (r - 1) % 10 + 1
+        while True:
+            r = (r7() - 1) * 7 + r7()
+            if r <= 40:
+                return (r - 1) % 10 + 1
