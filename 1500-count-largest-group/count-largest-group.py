ee5fee9331a0d22b63bb2b3a061f99121464c2ba
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-05-02 15:02:09 -0400

    Time: 45 ms (48.63%) | Memory: 17.7 MB (86.78%) - LeetSync

--- 1500-count-largest-group/count-largest-group.py
+++ 1500-count-largest-group/count-largest-group.py
@@ -1,20 +1,7 @@
-from collections import defaultdict
+from collections import Counter
 
 class Solution:
     def countLargestGroup(self, n: int) -> int:
-        freq = defaultdict(int)
-
-        for i in range(1, n + 1):
-            digit_sum = sum(int(digit) for digit in str(i))
-            freq[digit_sum] += 1
-
-        max_freq = 0
-        if freq: # Handle case where n=0, though constraints say n>=1
-            max_freq = max(freq.values())
-
-        count = 0
-        for group_size in freq.values():
-            if group_size == max_freq:
-                count += 1
-
-        return count
+        cnt = Counter(sum(map(int, str(i))) for i in range(1, n+1))
+        M = max(cnt.values())
+        return sum(v == M for v in cnt.values())
