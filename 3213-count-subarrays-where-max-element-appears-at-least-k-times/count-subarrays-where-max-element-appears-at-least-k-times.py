d58e78526f32a5dc7eb3a41a3a6ac3a8b020bbc9
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-05-02 14:47:38 -0400

    Time: 85 ms (48.06%) | Memory: 29.6 MB (74.58%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -16,13 +16,6 @@ class Solution:
                     window -= 1
                 left += 1
 
-            # When the window [left, right] contains at least k occurrences of 'top',
-            # any subarray ending at 'right' and starting from index 0 up to 'left - 1'
-            # is a valid subarray. The number of such starting positions is 'left'.
-            # If the `while` loop condition was `window >= k` and we exit it, it means
-            # the current window `[left, right]` has `window < k`.
-            # The valid subarrays ending at `right` are those starting from index 0 up to `left - 1`.
-            # The number of such subarrays is `left`.
             count += left
 
         return count
