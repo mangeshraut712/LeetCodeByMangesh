ef979d90db0e6ac3bfec868b8e006d9f0fa33733
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-05-02 03:46:26 -0400

    Time: 55 ms (29.52%) | Memory: 20.6 MB (33.26%) - LeetSync

--- 382-linked-list-random-node/linked-list-random-node.py
+++ 382-linked-list-random-node/linked-list-random-node.py
@@ -1,59 +1,14 @@
 import random
-from typing import Optional
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 
 class Solution:
-    """
-    Initializes the object with the head of the singly-linked list.
-    Provides a method to return a random node's value with equal probability.
-    """
-    def __init__(self, head: Optional[ListNode]):
-        """
-        Stores the head of the linked list.
+    def __init__(self, head):
+        self.h, self.rr = head, random.randrange
 
-        Args:
-            head: The head node of the singly-linked list.
-        """
-        self.head = head
-
-    def getRandom(self) -> int:
-        """
-        Chooses a node randomly from the list and returns its value.
-        All nodes have an equal probability of being chosen.
-        Uses Reservoir Sampling to handle potentially large linked lists
-        without storing all node values.
-
-        Returns:
-            The value of a randomly selected node.
-        """
-        random_value = 0  # Initialize with a default value (will be overwritten)
-        current_node = self.head
-        count = 0  # Counter for the number of nodes visited
-
-        # Iterate through the linked list using Reservoir Sampling
-        while current_node:
-            count += 1  # Increment the node count
-
-            # With probability 1/count, decide to keep the current node's value
-            # as the potential random value.
-            # random.randint(1, count) generates an integer from 1 to count (inclusive).
-            # If it's 1, we select the current node.
-            if random.randint(1, count) == 1:
-                random_value = current_node.val
-
-            # Move to the next node
-            current_node = current_node.next
-
-        # After iterating through the entire list, random_value holds the value
-        # of a randomly selected node with equal probability.
-        return random_value
-
-
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(head)
-# param_1 = obj.getRandom()
+    def getRandom(self):
+        node, ans, i = self.h, 0, 1
+        while node:
+            if self.rr(i) == 0:
+                ans = node.val
+            node = node.next
+            i += 1
+        return ans
