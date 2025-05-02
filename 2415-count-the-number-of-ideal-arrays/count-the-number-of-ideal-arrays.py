1ac1474af71a34766941833d66277d529fc6cfb4
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-05-02 15:11:22 -0400

    Time: 47 ms (68.60%) | Memory: 18.2 MB (95.93%) - LeetSync

--- 2415-count-the-number-of-ideal-arrays/count-the-number-of-ideal-arrays.py
+++ 2415-count-the-number-of-ideal-arrays/count-the-number-of-ideal-arrays.py
@@ -1,40 +1,32 @@
+import sys
+from math import log2
+from math import comb
+
 class Solution:
     MOD = 10**9 + 7
 
     def idealArrays(self, n: int, maxValue: int) -> int:
-        mod = self.MOD
-        # linear sieve for smallest prime factor
         mind = [0] * (maxValue + 1)
-        primes = []
-        for i in range(2, maxValue + 1):
-            if mind[i] == 0:
-                mind[i] = i
-                primes.append(i)
-            for p in primes:
-                ip = i * p
-                if ip > maxValue or p > mind[i]:
-                    break
-                mind[ip] = p
+        for p in range(2, maxValue + 1):
+            if mind[p] == 0:
+                for i in range(p, maxValue + 1, p):
+                    if mind[i] == 0:
+                        mind[i] = p
 
-        # precompute inv[1..L] and C[k] = C(n+k-1, k)
-        L = (maxValue.bit_length())
-        inv = [1] * (L + 1)
-        for i in range(2, L + 1):
-            inv[i] = mod - (mod // i) * inv[mod % i] % mod
-        C = [1] * (L + 1)
-        for k in range(1, L + 1):
-            C[k] = C[k - 1] * (n + k - 1) % mod * inv[k] % mod
+        maxPow = int(log2(maxValue)) + 1
+        C = [1] * (maxPow + 1)
+        for i in range(1, maxPow + 1):
+            C[i] = comb(n + i - 1, i) % self.MOD
 
         ans = 0
-        for v in range(1, maxValue + 1):
-            x = v
-            res = 1
+        for i in range(1, maxValue + 1):
+            x, prod = i, 1
             while x > 1:
-                p = mind[x]
-                e = 0
+                p, exp = mind[x], 0
                 while x % p == 0:
                     x //= p
-                    e += 1
-                res = res * C[e] % mod
-            ans += res
-        return ans % mod
+                    exp += 1
+                prod = prod * C[exp] % self.MOD
+            ans = (ans + prod) % self.MOD
+
+        return ans
