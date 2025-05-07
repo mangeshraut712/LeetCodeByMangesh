35e4dc92261f7bc4015ac852d3e48d37ac2cecac
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-05-06 20:44:55 -0400

    Time: 39 ms (71.59%) | Memory: 21.2 MB (6.24%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,18 +1,30 @@
-from typing import List
-
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        a = list(s)
-        st = []
-        ap, pop = st.append, st.pop
-        for i, c in enumerate(a):
+        res = []
+        ap = res.append
+        bal = 0
+
+        # First pass: drop invalid ')'
+        for c in s:
             if c == '(':
-                ap(i)
+                bal += 1
+                ap(c)
             elif c == ')':
-                if st:
-                    pop()
+                if bal:
+                    bal -= 1
+                    ap(c)
+            else:
+                ap(c)
+
+        # Second pass: drop excess '(' from the end
+        if bal:
+            out = []
+            ap2 = out.append
+            for c in reversed(res):
+                if c == '(' and bal:
+                    bal -= 1
                 else:
-                    a[i] = ''
-        for i in st:
-            a[i] = ''
-        return ''.join(a)
+                    ap2(c)
+            return "".join(reversed(out))
+
+        return "".join(res)
