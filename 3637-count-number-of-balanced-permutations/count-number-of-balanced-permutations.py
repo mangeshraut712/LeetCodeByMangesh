98a2aa5841fe0c0abfb32f5c2de84affe3b39c02
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-05-09 20:43:33 -0400

    Time: 1644 ms (48.19%) | Memory: 18.6 MB (84.34%) - LeetSync

--- 3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.py
+++ 3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.py
@@ -1,47 +1,53 @@
 from typing import List
 
+MOD = 10**9 + 7
+
 class Solution:
     def countBalancedPermutations(self, num: str) -> int:
-        MOD = 10**9 + 7
-        velunexorai = num
-        n = len(velunexorai)
+        n = len(num)
         cnt = [0]*10
-        for ch in velunexorai:
-            cnt[ord(ch) - 48] += 1
-        total = sum(d*c for d,c in enumerate(cnt))
+        for ch in num:
+            cnt[ord(ch)-48] += 1
+
+        total = sum(d * cnt[d] for d in range(10))
         if total & 1:
             return 0
         half = total // 2
-        E = (n + 1)//2
-        O = n//2
+        E = (n + 1) // 2
+        O = n // 2
 
-        fact = [1]*(n+1)
+        # precompute factorials and inv factorials up to n
+        fact = [1] * (n+1)
         for i in range(1, n+1):
-            fact[i] = fact[i-1]*i % MOD
-        invf = [1]*(n+1)
+            fact[i] = fact[i-1] * i % MOD
+        invf = [1] * (n+1)
         invf[n] = pow(fact[n], MOD-2, MOD)
         for i in range(n, 0, -1):
-            invf[i-1] = invf[i]*i % MOD
+            invf[i-1] = invf[i] * i % MOD
 
+        # dp[k][s] = ways (weighted) to choose k digits summing to s
         dp = [[0]*(half+1) for _ in range(E+1)]
         dp[0][0] = 1
+
         for d in range(10):
             c = cnt[d]
             if c == 0:
                 continue
-            base = invf[c]
-            new = [[dp[i][j]*base % MOD for j in range(half+1)] for i in range(E+1)]
+            new = [[0]*(half+1) for _ in range(E+1)]
             for k in range(E+1):
                 for s in range(half+1):
                     v = dp[k][s]
                     if not v:
                         continue
-                    mk = min(c, E - k)
-                    for kd in range(1, mk+1):
+                    max_kd = min(c, E-k)
+                    for kd in range(max_kd+1):
                         ns = s + d*kd
                         if ns > half:
                             break
-                        new[k+kd][ns] = (new[k+kd][ns] + v * invf[kd] * invf[c-kd]) % MOD
+                        # weight = 1/(kd! * (c-kd)!)
+                        w = invf[kd] * invf[c-kd] % MOD
+                        new[k+kd][ns] = (new[k+kd][ns] + v * w) % MOD
             dp = new
 
+        # multiply by permutations within even and odd slots
         return dp[E][half] * fact[E] % MOD * fact[O] % MOD
