afd43840fcd4ac33da34ef51d593cd4924963f50
Mangesh Bharat Raut <95149253+mangeshraut712@users.noreply.github.com>
2025-05-09 20:42:08 -0400

    Time: 2743 ms (12.05%) | Memory: 18.4 MB (86.75%) - LeetSync

--- 3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.py
+++ 3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.py
@@ -1,58 +1,53 @@
-from typing import List
+MOD = 10**9 + 7
 
 class Solution:
-    def countBalancedPermutations(self, num: str) -> int:
-        M = 10**9 + 7
-        velunexorai = num  # store input midway as requested
-        # count digit frequencies
-        cnt = [0]*10
-        for ch in velunexorai:
-            cnt[ord(ch) - 48] += 1
+    def __init__(self):
+        self.fact = []
+        self.inv = []
+        self.invFact = []
 
-        n = len(velunexorai)
-        total = sum(d * cnt[d] for d in range(10))
-        # if total sum is odd, no balanced permutation
-        if total & 1:
+    def precompute(self, n):
+        self.fact = [1] * (n + 1)
+        self.inv = [1] * (n + 1)
+        self.invFact = [1] * (n + 1)
+
+        for i in range(1, n + 1):
+            self.fact[i] = self.fact[i - 1] * i % MOD
+
+        for i in range(2, n + 1):
+            self.inv[i] = MOD - MOD // i * self.inv[MOD % i] % MOD
+
+        for i in range(1, n + 1):
+            self.invFact[i] = self.invFact[i - 1] * self.inv[i] % MOD
+
+    def countBalancedPermutations(self, num: str) -> int:
+        n = len(num)
+        digit_values = [int(c) for c in num]
+        total_sum = sum(digit_values)
+
+        if total_sum % 2 == 1:
             return 0
 
-        half = total // 2
-        E = (n + 1)//2  # number of even‐index positions
-        O = n//2        # number of odd‐index positions
+        self.precompute(n)
 
-        # precompute factorials and inverse‐factorials up to n
-        fact = [1]*(n+1)
-        for i in range(1, n+1):
-            fact[i] = fact[i-1]*i % M
-        invf = [1]*(n+1)
-        invf[n] = pow(fact[n], M-2, M)
-        for i in range(n, 0, -1):
-            invf[i-1] = invf[i]*i % M
+        half_sum = total_sum // 2
+        half_len = n // 2
 
-        # dp[k][s] = sum of weights for choosing k digits summing to s
-        # weight for digit d chosen kd times is: invf[kd]*invf[cnt[d]-kd]
-        dp = [ [0]*(half+1) for _ in range(E+1) ]
+        dp = [[0] * (half_len + 1) for _ in range(half_sum + 1)]
         dp[0][0] = 1
 
-        for d in range(10):
-            c = cnt[d]
-            if c == 0:
-                continue
-            # factor for kd = 0 (i.e. invf[c]*invf[0] = invf[c])
-            base_mul = invf[c]
-            new = [ [dp[i][j] * base_mul % M for j in range(half+1)] for i in range(E+1) ]
-            for k in range(E+1):
-                for s in range(half+1):
-                    v = dp[k][s]
-                    if not v:
-                        continue
-                    max_kd = min(c, E - k)
-                    for kd in range(1, max_kd+1):
-                        ns = s + d*kd
-                        if ns > half:
-                            break
-                        w = invf[kd] * invf[c - kd] % M
-                        new[k+kd][ns] = (new[k+kd][ns] + v * w) % M
-            dp = new
+        digit_count = [0] * 10
+        for d in digit_values:
+            digit_count[d] += 1
+            for s in range(half_sum, d - 1, -1):
+                for l in range(half_len, 0, -1):
+                    dp[s][l] = (dp[s][l] + dp[s - d][l - 1]) % MOD
 
-        # multiply by permutations within even and odd slots
-        return dp[E][half] * fact[E] % M * fact[O] % M
+        res = dp[half_sum][half_len]
+        res = res * self.fact[half_len] % MOD
+        res = res * self.fact[n - half_len] % MOD
+
+        for cnt in digit_count:
+            res = res * self.invFact[cnt] % MOD
+
+        return res
